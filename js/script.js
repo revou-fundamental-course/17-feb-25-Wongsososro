40156e6cdfac72c9410bb745ef2f1b0f116cf3d2
Wongsososro <briantayu57@gmail.com>
2025-02-26 18:57:13 +0700

    final-sefc

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
 // external js
+let currentIndex = 0;
+    const slides = document.querySelectorAll('.carousel img');
+    const totalSlides = slides.length;
+    const carousel = document.querySelector('.carousel');
+
+    function updateCarousel() {
+        carousel.style.transform = `translateX(${-currentIndex * 100}%)`;
+    }
+    
+    function nextSlide() {
+        currentIndex = (currentIndex + 1) % totalSlides;
+        updateCarousel();
+    }
+
+    function prevSlide() {
+        currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
+        updateCarousel();
+    }
+    setInterval(nextSlide, 3000);
